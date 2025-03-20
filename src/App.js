79ec44e505743a3b18b9fc436ed99963b170cff4
girish-shahapurkar <shahapurkargirish94@gmail.com>
2025-03-20 14:16:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Home from './Home';
+import SingleMovie from './SingleMovie'
+import Error from './Error';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import "./App.css"
+function App() {
+
+  
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='movie/:id' element={<SingleMovie/>}/>
+        <Route path="*" element={<Error/>}/>
+      </Routes>
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Name.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Name =(value)=>{
+
+    const data = 'girish'
+    return <>
+    <div>hi welcome to my channel</div>
+    <h1></h1>
+    </>
+}
+
+export default Name
